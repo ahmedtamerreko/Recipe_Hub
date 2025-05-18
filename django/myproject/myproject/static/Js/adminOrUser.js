88b3577cabe18fcb056648f248816455e89f2ece
Ahmed Tamer Reko <rekotamer4@gmail.com>
2025-05-19 00:42:00 +0300

    Delete django/myproject directory

--- django/myproject/myproject/static/Js/adminOrUser.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".Admin").addEventListener("click", function () {
-        window.location.href = "/admin-signin/";
-    });
-
-    document.querySelector(".User").addEventListener("click", function () {
-        window.location.href = "/user_signin/";
-    });
-});
