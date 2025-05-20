676e8728e51c4c2e90ab6281eabf39c66a3d3906
Ahmed Tamer Reko <rekotamer4@gmail.com>
2025-05-20 23:28:09 +0300

    Delete Recipe_Hub-main directory

--- Recipe_Hub-main/Recipes_main/Recipe_Hub-main/Recipe_Hub/django/myproject/pages/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserData
-from .models import AdminData
-from .models import Recipe
-# Register your models here.
-admin.site.register(UserData)
-admin.site.register(AdminData)
-admin.site.register(Recipe)
-
